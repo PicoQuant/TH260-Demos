ebcc787d724387888edc1acdb593405468d6d8c9
Steffen Rüttinger <devnull@snizzle.org>
2022-11-10 14:57:01 +0100

    removed Linux

--- Linux/driver/th260ioctl.h
+++
@@ -1,8 +0,0 @@
-
-#include <linux/ioctl.h>
-  
-#define IOCTL_GET_VERSION _IOR('t', 0, uint64_t)  //actually an address
-#define IOCTL_GET_SERIAL  _IOR('t', 1, uint64_t)  //actually an address
-#define IOCTL_GET_BAROFFS _IOR('t', 2, uint64_t)  //actually an address
-
-
